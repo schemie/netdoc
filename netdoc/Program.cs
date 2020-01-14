0dfde657008b0f99e97f1faa4d266733dc9094a0
unknown <matt.mckay@outlook.com>
2020-01-14 00:23:25 -0800

    config working

---
+++ netdoc/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace netdoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String config_file;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No argument provided. Default config file is being used.");
+                config_file = "config.json";
+            } else {
+                config_file = args[0];
+                Console.WriteLine($"Using configuration from {config_file}");
+            }
+
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(config_file, optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            Console.WriteLine(configuration["Settings:in_folder"].ToString());
+
+
+        }
+    }
+}
